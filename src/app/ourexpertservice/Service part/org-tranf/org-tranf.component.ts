9bb67fc81e9e710cd394382636926e159e916cda
Ajay jatwa <ajay.wagons@gmail.com>
2021-08-23 18:08:13 +0530

    change in home & service component

---
+++ src/app/ourexpertservice/Service part/org-tranf/org-tranf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-org-tranf',
+  templateUrl: './org-tranf.component.html',
+  styleUrls: ['./org-tranf.component.css']
+})
+export class OrgTranfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
