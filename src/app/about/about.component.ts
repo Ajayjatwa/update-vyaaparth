e07c3836c74f7039a207c99ab7e16ba631d5c209
Ajay jatwa <ajay.wagons@gmail.com>
2021-09-21 18:08:12 +0530

    add filter pipe

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public searchFilter: any = '';
+  query:string='';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  Students = [{
+    "imageUrl": "../../assets/img/about/tab-banner.jpg",
+    "name": "Nathaniel Graham",
+    "bio": "nathaniel.graham@example.com"
+  },
+  {
+    "imageUrl": "../../assets/img/home1/team-2.jpg",
+    "name": "Jason Ahlberg",
+    "bio": "nathaniel.graham@example.com"
+  },
+  {
+    "imageUrl": "../../assets/img/home1/team-1.jpg",
+    "name": "James Alt",
+    "bio": "nathaniel.graham@example.com"
+  },
+  {
+    "imageUrl": "../../assets/img/home1/team-2.jpg",
+    "name": "Emily Alverson",
+    "bio": "nathaniel.graham@example.com"
+  },
+  {
+    "imageUrl": "../../assets/img/home1/team-3.jpg",
+    "name": "Emily Alverson",
+    "bio": "nathaniel.graham@example.com"
+  },
+  {
+    "imageUrl": "../../assets/img/home1/team-4.jpg",
+    "name": "James Alt",
+    "bio": "nathaniel.graham@example.com"
+  },
+  {
+    "imageUrl": "../../assets/img/home1/team-1.jpg",
+    "name": "Emily Alverson",
+    "bio": "nathaniel.graham@example.com"
+  },
+
+]
+
 
 }
