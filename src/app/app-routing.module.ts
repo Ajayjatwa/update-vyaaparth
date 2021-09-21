b69eaa69cfb67153e8731971f03b157cd3147390
Ajay jatwa <ajay.wagons@gmail.com>
2021-09-21 10:14:23 +0530

    module add and footer making

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,103 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { CareersComponent } from './careers/careers.component';
-import { BusinessTransformationComponent } from './contant/business-transformation/business-transformation.component';
-import { DigitalTransformationComponent } from './contant/digital-transformation/digital-transformation.component';
-import { MarketingOperationsComponent } from './contant/marketing-operations/marketing-operations.component';
-import { MarketingStrategyComponent } from './contant/marketing-strategy/marketing-strategy.component';
-import { SeamlessComponent } from './contant/seamless/seamless.component';
-import { TransformationComponent } from './contant/transformation/transformation.component';
 import { FunctionsComponent } from './functions/functions.component';
 import { HeaderComponent } from './header/header/header.component';
 import { PageNotFoundComponent } from './header/page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
 import { IndustriesComponent } from './industries/industries.component';
-import { OurexpertserviceComponent } from './ourexpertservice/ourexpertservice.component';
-import { AppCoreTechComponent } from './ourexpertservice/Service part/app-core-tech/app-core-tech.component';
-import { BuildNextGenerationComponent } from './ourexpertservice/Service part/build-next-generation/build-next-generation.component';
-import { CustomerRelationshipComponent } from './ourexpertservice/Service part/customer-relationship/customer-relationship.component';
-import { DigitalStrategyComponent } from './ourexpertservice/Service part/digital-strategy/digital-strategy.component';
-import { LeadershipManagementComponent } from './ourexpertservice/Service part/leadership-management/leadership-management.component';
-import { HowOrganizationsComponent } from './ourexpertservice/Service part/org-tranf/How-Organizations/how-organizations/how-organizations.component';
-import { OrgTranfComponent } from './ourexpertservice/Service part/org-tranf/org-tranf.component';
-import { LeadershipAndManagementComponent } from './ourexpertservice/Service part/organization-performance/Leadership-Management/leadership-and-management/leadership-and-management.component';
-import { OrganizationPerformanceComponent } from './ourexpertservice/Service part/organization-performance/organization-performance.component';
-import { TransfoSerForMarkeOperationsComponent } from './ourexpertservice/Service part/transfo-ser-for-marke-operations/transfo-ser-for-marke-operations.component';
-import { Service1Component } from './ourexpertservice/service1/service1.component';
-import { ShareServiceComponent } from './share-service/share-service.component';
-import { SupplyChainComponent } from './supply-chain/supply-chain.component';
+
 
 const routes: Routes = [
   {path:'', redirectTo:'home', pathMatch:'full'},
-  {path:'home',
-  children:
-  [
-    {path:'', component:HomeComponent},
-    {path:'services', component: Service1Component},
-    {path:'tranformation', component:TransformationComponent},
-    {path:'digital', component:DigitalTransformationComponent},
-    {path:'marketing', component:MarketingOperationsComponent},
-    // {path:'marketingStrategy', component:MarketingStrategyComponent},
-  ]},
-  // changes
-
-
-  { path:'marketing', component: MarketingOperationsComponent},
-  // changes
+  {path:'home',component:HomeComponent},
+  // { path:'marketing', component: MarketingOperationsComponent},
   {path:'industries', component:IndustriesComponent },
   {path:'functions', component:FunctionsComponent},
-
-  {path:'ourexpertservice',
-  children:[
-    {path:'', component:OurexpertserviceComponent},
-    {path:'buld-next-generation', component:BuildNextGenerationComponent},
-    {path:'supply-chain',
-    children:[
-      {path:'',component:SupplyChainComponent},
-      {path:'share-service', component:ShareServiceComponent},
-    ]},
-    {path:'Organizational-Transformation',
-    children:[
-      {path:'', component:OrgTranfComponent},
-      {path:'how-orgnization', component:HowOrganizationsComponent},
-    ]},
-    {path:'application core technology',
-    children:[
-      {path:'', component:AppCoreTechComponent},
-      {path:'digital-strategy',component:DigitalStrategyComponent },
-      {path:'leadership-management', component:LeadershipManagementComponent},
-      {path:'Customer-Relationship',component:CustomerRelationshipComponent},
-      {path:'organization-performance',
-      children:[
-        {path:'', component:OrganizationPerformanceComponent},
-        {path:'leadership-and-management', component:LeadershipAndManagementComponent}
-      ]},
-    ]},
-    // {path:'digital-strategy', component:DigitalStrategyComponent},
-    // {path:'leadership-management', component:LeadershipManagementComponent},
-
-  ]},
-  {path:'business-transformation',children:[
-    {path:'', component: BusinessTransformationComponent},
-    {path:'marketingStrategy', component: MarketingStrategyComponent},
-    { path:'seamless', component:SeamlessComponent},
-    {path:'marketing', component:MarketingOperationsComponent},
-  ]},
-  {path:'tranformation-service-for-marketing', children:[
-
-    {path:'', component:TransfoSerForMarkeOperationsComponent},
-    {path:'marketing', component: MarketingOperationsComponent},
-    {path:'tranformation', component:TransformationComponent},
-    {path:'digital', component:DigitalTransformationComponent},
-  ]},
   {path:'about', component:AboutComponent},
-  // { path:'digital-strategy',component:DigitalStrategyComponent},
-  // {path:'organization-performance', component:OrganizationPerformanceComponent},
-  // {path:'leadership-management', component:LeadershipManagementComponent},
-  // {path:'Customer-Relationship',component:CustomerRelationshipComponent},
-  { path:'seamless', component:SeamlessComponent},
   {path:'career', component:CareersComponent},
-  {path:'**', component:PageNotFoundComponent},
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
